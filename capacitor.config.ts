0f2a869d4a4df7359c53cfd5e55dd1c379869ac6
Emmanuel Chavez <emmanuel.chavez.condor@gmail.com>
2023-09-30 15:27:45 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.origamicode.testassessment.ecn',
+  appName: 'TestAssessmentOrigamiCode',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
