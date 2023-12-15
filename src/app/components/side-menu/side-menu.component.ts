686a7fe7422d76cbeebe2be42803f4955ae6df3f
Emmanuel Chavez <emmanuel.chavez.condor@gmail.com>
2023-12-14 23:15:57 -0600

    wip: add side menu component

---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { NavigationItems } from 'src/shared/enums/navigation.enum';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SideMenuComponent implements OnInit {
+  activePage = 'Home';
+  appPages = [
+    { title: 'Home', url: NavigationItems.home, icon: 'home' },
+    { title: 'Profile', url: NavigationItems.profile, icon: 'person' }
+  ]
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
