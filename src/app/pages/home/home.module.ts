8e3c987889b9d4fb7fdfaa1020612b42daafbdc0
Emmanuel Chavez <emmanuel.chavez.condor@gmail.com>
2023-12-18 19:28:07 -0600

    chore: call to date time picker component & main header component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { DateTimePickerComponent } from 'src/app/components/date-time-picker/date-time-picker.component';
+import { MainHeaderComponent } from 'src/app/components/headers/main-header/main-header.component';
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
+    MainHeaderComponent,
+    DateTimePickerComponent,
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { DateHelper } from 'src/helpers';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  quoteForm: UntypedFormGroup;
+  dateValue = new Date(Date.now()).toISOString();
+  format = 'dd/MM/yyyy';
 
-  constructor() { }
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private dateHelper: DateHelper
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
+
+  initForm() {
+    this.quoteForm = this.formBuilder.group({
+      quotesDate: [this.dateValue || ''],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onChange(event: any) {
+    this.dateValue = event;
+    const newDate = this.dateHelper.formatDate(this.dateValue, this.format);
+  }
+
+  sendData() { }
 
 }
