237b41c981b0d9331e559bf980f7f02734fc135d
Emmanuel Chavez <emmanuel.chavez.condor@gmail.com>
2023-12-20 17:59:49 -0600

    chore: import DataTtableComponent & AppointmentCoreModule

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,9 +6,11 @@ import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { DataTableComponent } from 'src/app/components/data-table/data-table.component';
 import { DateTimePickerComponent } from 'src/app/components/date-time-picker/date-time-picker.component';
 import { MainHeaderComponent } from 'src/app/components/headers/main-header/main-header.component';
 import { HomePage } from './home.page';
+import { AppointmentCoreModule } from 'src/app/modules/appoiment/appointment.module';
 
 @NgModule({
   imports: [
@@ -19,6 +21,8 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule,
     ReactiveFormsModule,
+    DataTableComponent,
+    AppointmentCoreModule
   ],
   declarations: [HomePage]
 })
